57c8d4e1d34e8351b5cbc8304e216d961bd9242c
RebeccaNavel <49717073+RebeccaNavel@users.noreply.github.com>
2019-05-13 22:33:17 +0200

    update

--- second_steps.py
+++ second_steps.py
@@ -6,16 +6,64 @@ sat_database = {"METEOSAT" : 3000,
                 "MODIS"    : 500
                }
 
+print (sat_database)
+
 # The dictionary above contains the names and spatial resolutions of some satellite systems.
 
 # 1) Add the "GOES" and "worldview" satellites with their 2000/0.31m resolution to the dictionary [2P]
+sat_database ["GOES"] = 2000
+sat_database ["worldview"] = 0.31
 
 print("I have the following satellites in my database:")
+print (sat_database)
 
 # 2) print out all satellite names contained in the dictionary [2P]
 
+print (sat_database.keys ())
+
 # 3) Ask the user to enter the satellite name from which she/he would like to know the resolution [2P]
+
+answer = input ("Please name the satellite of which you want to know the resolution.")
+
+if answer == "METEOSAT":
+    print ("METEOSAT has a resolution of 3000 Meters.")
+    
+elif answer == "LANDSAT":
+    print ("LANDSAT has a resolution of 30 Meters.")
+    
+elif answer == "MODIS":
+    print ("MODIS has a resolution of 500 Meters.")
+    
+elif answer == "GOES":
+    print ("GOES has a resolution of 2000 Meters.")
+    
+elif answer == "worldview":
+    print ("worldview has a resolution of 0.31 Meters.")
+    
+else:
+    print ("I can't find the satellite, you are searching for. Please check your spelling and note the case sensitivity. Also note that no spaces are allowed.")
 
 # 4) Check, if the satellite is in the database and inform the user, if it is not [2P]
 
+if answer in sat_database:
+    print ("The satellite is in the database.")
+    
+else: 
+    print ("The satellite is not in the database.")
+
 # 5) If the satellite name is in the database, print a meaningful message containing the satellite name and it's resolution [2P] 
+
+if answer == "METEOSAT":
+    print("The satellite METEOSAT has the following resolution:",sat_database[answer],".")
+    
+elif answer == "LANDSAT":
+    print("The satellite LANDSAT has the following resolution:",sat_database.get(answer),".")
+    
+elif answer == "MODIS":
+    print("The satellite MODIS has the following resolution:",sat_database["MODIS"],".")
+    
+elif answer == "GOES":
+    print("The satellite GOES has the following resolution:",sat_database.get("GOES"),".")
+    
+elif answer == "WORLDVIEV":  #again the question, is it possible to do it like upper(worldview)?
+    print("The satellite worldview has the following resolution: 0.31m")
